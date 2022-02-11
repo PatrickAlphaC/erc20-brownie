1ac70bc55be8164daf066448dd22f935d315c2ea
PatrickAlphaC <collinspatrickkevin@gmail.com>
2022-02-11 11:03:23 -0700

    updated for 0.8.0 of solidity

--- contracts/TokenERC20.sol
+++ contracts/TokenERC20.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.4.22 <0.6.0;
+pragma solidity ^0.6.0;
 
 interface tokenRecipient { 
     function receiveApproval(address _from, uint256 _value, address _token, bytes calldata _extraData) external; 
